25a7c2eda7eb1df2a7261749834790014b41d799
loniki987 <loniki987@gmail.com>
2025-01-14 14:30:01 +0200

    Save&Data Position
    כרגע רק מיקום זה שומר

--- Assets/Scripts/Scripts_Nikki/PlayerControllerNiki.cs
+++ Assets/Scripts/Scripts_Nikki/PlayerControllerNiki.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerNiki : MonoBehaviour
+public class PlayerControllerNiki : MonoBehaviour, IDataPersistenceNiki
 {
+    public void LoadData(DataNiki data)
+    {
+        this.transform.position = data.playerPosition;
+    }
+    public void SaveData(ref DataNiki data)
+    {
+        data.playerPosition = this.transform.position;
+    }
+    public float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+        else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
     }
 }
