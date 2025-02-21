0882dd51bfb99ac1255495f30a3d955a15006539
YoDavid <cyohanovdavid@gmail.com>
2025-02-21 15:53:48 +0200

    Test_With_Kit

--- Assets/Scripts/Scripts_David/Player/PlayerMovementController.cs
+++ Assets/Scripts/Scripts_David/Player/PlayerMovementController.cs
@@ -10,6 +10,8 @@ public class PlayerMovementController : MonoBehaviour
     public bool isGrounded = false;
     public bool isDashing = false;
     public bool isCollidingWithWall = false;
+    public bool isMovingRight = false;
+    public bool isMovingLeft = false;
 
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
@@ -61,8 +63,23 @@ public class PlayerMovementController : MonoBehaviour
         float move = 0f;
         if (!playerAttackController.IsAttacking)
         {
-            if (Input.GetKey(KeyCode.A)) move = -1f;
-            else if (Input.GetKey(KeyCode.D)) move = 1f;
+            if (Input.GetKey(KeyCode.A))
+            {
+                move = -1f;
+                isMovingLeft = true;
+                isMovingRight = false;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                move = 1f;
+                isMovingLeft = false;
+                isMovingRight = true;
+            }
+            else
+            {
+                isMovingLeft = false;
+                isMovingRight = false;
+            }
         }
 
         Move(move);
@@ -79,6 +96,7 @@ public class PlayerMovementController : MonoBehaviour
 
         playerAnimationController.UpdateAnimationStates(move, isGrounded, isDashing);
     }
+
 
     private void Move(float move)
     {
