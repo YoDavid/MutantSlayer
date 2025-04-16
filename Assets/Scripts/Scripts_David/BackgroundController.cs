c1a53c4726fbdeef178c7abfa622578fcf4222a7
loniki987 <loniki987@gmail.com>
2025-04-16 21:00:57 +0300

    קצת עבדתי ותיקנתי דברים

--- Assets/Scripts/Scripts_David/BackgroundController.cs
+++ Assets/Scripts/Scripts_David/BackgroundController.cs
@@ -19,7 +19,7 @@ public class BackgroundController : MonoBehaviour
     {
         float distance = cam.transform.position.x * parallaxEffectX;
         float distanceY = cam.transform.position.y * 1 * parallaxEffectY;
-        transform.position = new Vector3(startPosX + distance, /*startPosY + distanceY*/transform.position.y , transform.position.z);
+        transform.position = new Vector3(startPosX + distance, startPosY + distanceY , transform.position.z);
         
         float movement = cam.transform.position.x * (1 - parallaxEffectX);
         if (movement > startPosX + length)
