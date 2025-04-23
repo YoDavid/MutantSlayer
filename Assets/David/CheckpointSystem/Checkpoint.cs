53520a7593176d82bfa67ba055f97ed7c4cd822f
YoDavid <cyohanovdavid@gmail.com>
2025-04-23 20:52:59 +0300

    adstments

--- Assets/David/CheckpointSystem/Checkpoint.cs
+++ Assets/David/CheckpointSystem/Checkpoint.cs
@@ -4,6 +4,7 @@ public class Checkpoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Arrived to checkpoint");
         if (other.CompareTag("Player"))
         {
             var dataCollector = other.GetComponent<PlayerDataCollector>();
